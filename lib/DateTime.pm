69820da2a0e0186fbd109337b4f5c413790132b6
Dave Rolsky <autarch@urth.org>
2003-07-30 21:25:05 -0800

    import DateTime 0.1502 from CPAN
    git-cpan-module:   DateTime
    git-cpan-version:  0.1502
    git-cpan-authorid: DROLSKY
    git-cpan-file:     authors/id/D/DR/DROLSKY/DateTime-0.1502.tar.gz

--- lib/DateTime.pm
+++ lib/DateTime.pm
@@ -6,24 +6,29 @@ use vars qw($VERSION);
 
 BEGIN
 {
-    $VERSION = '0.1501';
+    $VERSION = '0.1502';
 
     my $loaded = 0;
     unless ( $ENV{PERL_DATETIME_PP} )
     {
-#        if ( $] >= 5.006 )
-#        {
-#            require XSLoader;
-#            XSLoader::load( 'DateTime', $DateTime::VERSION );
-#        }
-#        else
-#        {
-            require DynaLoader;
-            @DateTime::ISA = 'DynaLoader';
-            DateTime->bootstrap( $DateTime::VERSION );
-#        }
+	eval
+	{
+	    if ( $] >= 5.006 )
+	    {
+		require XSLoader;
+		XSLoader::load( 'DateTime', $DateTime::VERSION );
+	    }
+	    else
+	    {
+		require DynaLoader;
+		@DateTime::ISA = 'DynaLoader';
+		DateTime->bootstrap( $DateTime::VERSION );
+	    }
+	};
 
-        $loaded = 1;
+	die $@ if $@ && $@ !~ /object version/;
+
+        $loaded = 1 unless $@;
     }
 
     if ($loaded)
